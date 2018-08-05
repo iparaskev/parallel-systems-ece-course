812d206223a25fc6303c7c39b472d00e08c45a8b
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2018-08-05 17:09:59 +0300

    add new functions

--- headers/helper.h
+++ headers/helper.h
@@ -7,5 +7,10 @@ Sparse_half** create_adjacency(int N, int rows,
 		               Sparse_list *dataset, int **row_sums);
 int count_elements(int *rows, Sparse_list *array);
 double norm(double *a, double *b, int N);
+void save_results(double *x, int N);
+void validate(double *x, int N, char *filename);
+double now();
+double elapsed_time(double start, double end);
+void normalize(double **x_init, int rows);
 
 #endif
