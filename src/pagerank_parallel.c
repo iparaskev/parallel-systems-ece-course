0a9f12e30b28b26bcc282559096f534952eeb0ea
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2018-08-06 18:05:59 +0300

    continius really slow

--- src/pagerank_parallel.c
+++ src/pagerank_parallel.c
@@ -4,144 +4,186 @@
 #include "constants.h"
 #include <pthread.h>
 
-/* Global constants */
-double *x_new;
-Sparse_half *row_A;
-int row_id;
+#define NUM_THREADS 1
 
-typedef struct thread_args
+/* Global variables */
+double *x_new;        // The pagerank vector.
+double a_ii;          // Variable for division at Gauss Seidel.
+double x_i;           // The variable used for updating a row in the vector.
+int row_elements;     // The number of elements of the current row.
+int row_id;           // The current row index.
+int N;		      // The number of rows.
+Sparse_half *row_A;   // The current row.
+int elements_per_thr; // How many elements any thread will have.
+int done;             // A counter for the finished threads.
+int ready;            // A counter for the threads that are ready to start.
+
+/* Mutexes and condition variables.*/
+static pthread_mutex_t mutex_start = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutex_done = PTHREAD_MUTEX_INITIALIZER;
+
+static pthread_cond_t cond_main = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t cond_thr = PTHREAD_COND_INITIALIZER;
+
+/* Function to print error and exit the process for visual reasons */
+void 
+err_exit(char *arg)
 {
-	int id;
-	int N;
-	double *a_i, *x_i;
-} in_args;
+	fprintf(stderr, "%s\n", arg);
+	exit(1);
+}
 
 void*
 compute_row(void *args)
 {
-	in_args thread_args = *((in_args *) args);
-	int start = thread_args.id * thread_args.N; // The start of iterations
-	int end = start + thread_args.N; // The end of iterations
-	double x_i = 0.;
-	double a_i = *(thread_args.a_i);
 
+	int id = (long) args;
+	int s;
 	/*inside loop variables*/
 	double value;
-	int real_col;
-	//printf("Thread %d N %d\n", thread_args.id, thread_args.N);
-	for (int i = start; i < end; i++)
+	int real_col, start, end;
+	for(;;)
 	{
-		/* Get the real values.*/
-		real_col = row_A[i].col;
-		value = row_A[i].value;
+		/* Put thread to sleep until main is ready.*/
+		s = pthread_mutex_lock(&mutex_start);
+		if (s != 0)
+			err_exit("Error at mutex lock");
+
+		ready++;
+		//printf("thread bef %d %d\n", id, ready);
 		
-		a_i += value * (real_col == row_id);
+		s = pthread_cond_wait(&cond_thr, &mutex_start);
+		if (s != 0)
+			err_exit("Error at mutex lock");
+		//printf("thread after %d %d\n", id, ready);
+		
+		s = pthread_mutex_unlock(&mutex_start);
+		if (s != 0)
+			err_exit("Error at mutex unlock");
 
-		/* Update the sum.*/
-		x_i += value * x_new[real_col] * (real_col != row_id);
+		
+		/* Compute the local sum of elements.*/
+		start = id * elements_per_thr;
+		if (id == NUM_THREADS - 1)
+			end = row_elements;
+		else
+			end = start + elements_per_thr;
+
+		double local_sum = 0;
+		double local_ai = 0;
+		for (int i = start; i < end; i++)
+		{
+			/* Get the real values.*/
+			real_col = row_A[i].col;
+			value = row_A[i].value;
+			
+			local_ai += value * (real_col == row_id);
+
+			/* Update the sum.*/
+			local_sum += value \
+				     * x_new[real_col] \
+				     * (real_col != row_id);
+		}
+
+		/* Update global x_i, a_i and done variables.*/
+		s = pthread_mutex_lock(&mutex_done);
+		if (s != 0)
+			err_exit("Error at mutex lock");
+
+		x_i -= local_sum;
+		a_ii += local_ai;
+		done++;
+
+		/* Wake up main thread to update the vector.*/
+		if (done == NUM_THREADS)
+			s = pthread_cond_signal(&cond_main);
+
+		s = pthread_mutex_unlock(&mutex_done);
+		if (s != 0)
+			err_exit("Error at mutex unlock");
+
+		if (row_id == (N - 1))
+			break;
 	}
-
-	*(thread_args.a_i) = a_i;
-	*(thread_args.x_i) = x_i;
+	
 	return NULL;
 }
 
 double*
 gs_mult_par(Sparse_half **A, double **x, double *b, int rows, int *row_sums)
 {
-	double *x_old;
 	x_new = *x;
+	double *x_old;
 	x_old = malloc(rows * sizeof *x_old);
 	for (int i = 0; i < rows; i++)
 		x_old[i] = x_new[i];
 
-	double a_ii, value, x_i;
-	int real_col;
-
-	pthread_t *threads;  // The threads pointer to be used
 	int s; // Variable for return values of thread functions
-	int elementes_per_thr; // how many elements per thread
-	in_args *input_args;
-	double t;
-	double init_sum = 0.;
-	double calls_sum = 0.;
-	double results_sum = 0.;
+
+	/* Initialize threads.*/
+	pthread_t threads[NUM_THREADS];
+
+	/* Threads call*/
+	ready = 0;
+	for (int i = 0; i < NUM_THREADS; i++)
+	{
+		pthread_create(&threads[i], NULL, compute_row, (void *) (long) i);
+		if (s != 0)
+			err_exit("pthread create.");
+	}
 
 	for (int row = 0; row < rows; row++)
 	{
-		t = now();
-		//printf("row %d N %d\n", row, row_sums[row]);
+		//printf("row %d\n", row);
 		a_ii = 1.;
+		row_id = row;
 		x_i = b[row];
 		row_A = A[row];
+		row_elements = row_sums[row];
+		elements_per_thr = row_elements / NUM_THREADS;
 
-		/* Dynamic number of threads to not burn resourcea */
-		int num_threads = 4;
-		threads = malloc(num_threads * sizeof *threads);
-		if (threads == NULL)
+		/* Wake threads to compute the element.*/
+		for (;;)
 		{
-			perror("Malloc at threads initialization.");
-			exit(0);
+			//puts("1");
+			s = pthread_mutex_lock(&mutex_start);
+			if (ready == NUM_THREADS)
+			{
+				ready = 0;
+				s = pthread_cond_broadcast(&cond_thr);
+				if (s != 0)
+					err_exit("pthread condition broadcast.");
+				s = pthread_mutex_unlock(&mutex_start);
+				break;
+			}
+			s = pthread_mutex_unlock(&mutex_start);
 		}
-		
-		/* Dynamic creation of threads arguments */
-		input_args = malloc(num_threads * sizeof *input_args);
-		if (input_args == NULL)
+		//puts("2");
+
+		/* Go to sleep until the other threads finish their job.*/
+		s = pthread_mutex_lock(&mutex_done);
+		if (s != 0)
+			err_exit("pthread mutex lock.");
+		while (done != NUM_THREADS)
 		{
-			perror("Malloc at threads initialization.");
-			exit(0);
+			s = pthread_cond_wait(&cond_main, &mutex_done);
+			if (s != 0)
+				err_exit("pthread condition wait.");
 		}
+		done = 0;
+		s = pthread_mutex_unlock(&mutex_done);
+		if (s != 0)
+			err_exit("pthread mutex unlock.");
 
-		elementes_per_thr = row_sums[row] / num_threads;
-		init_sum += now() - t;
-
-		t = now();
-		/* Call thread function.*/
-		for (int i = 0; i < num_threads; i++)
-		{
-			/* Give values to every thread arguments */
-			input_args[i].id = i;
-			input_args[i].a_i = &a_ii;
-			//input_args[i].x_i = malloc(sizeof *input_args[i].x_i);
-
-		//	/* Handle the case when row_sums isn't divisible from
-		//	 * num_threads.*/
-		//	if (i == num_threads)
-		//		input_args[i].N = row_sums[row] - i*elementes_per_thr;
-		//	else
-		//		input_args[i].N = elementes_per_thr;
-
-		//	s = pthread_create(&threads[i], NULL,
-		//		           compute_row, (void *) &input_args[i]);
-		//	if (s != 0)
-		//	{
-		//		fprintf(stderr, "Error at thread creation.\n");
-		//		exit(1);
-		//	}
-		}
-		calls_sum += now() - t;
-
-		/* Get thread results.*/
-		t = now();
-		//for (int i = 0; i < num_threads; i++)
-		//{
-		//	s = pthread_join(threads[i], NULL);
-		//	if (s != 0)
-		//	{
-		//		fprintf(stderr, "Error at thread results.\n");
-		//		exit(1);
-		//	}
-		//	x_i -= *(input_args[i].x_i);
-		//	free(input_args[i].x_i);
-		//}
-		results_sum += now() - t;
-
-		/* Clean up */
-		free(threads);
-		free(input_args);
+		/* Update element.*/
 		x_new[row] = x_i / a_ii;
+		//puts("3");
+	
+		
 	}
-	printf("Init_time %f Calls_time %f Res_time %f\n", init_sum, calls_sum, results_sum);
+
+	for (int i = 0; i < NUM_THREADS; i++)
+		pthread_join(threads[i], NULL);
 	
 	return x_old;
 }
@@ -156,6 +198,7 @@ pagerank_par(Sparse_half **adjacency, int rows, int *row_sums)
 	 */
 
 	/* Initialize b vector and initial pagerank*/
+	N = rows;
 	double *b, *x, *x_old;
 	b = malloc(rows * sizeof *b);
 	if ((x = malloc(rows * sizeof *x)) == NULL)
@@ -172,7 +215,7 @@ pagerank_par(Sparse_half **adjacency, int rows, int *row_sums)
 	int iterations = 0;
 	double delta = 1;
 	double t, t_s;
-	while ( iterations < MAX_ITERATIONS)
+	while ( delta > TOL && iterations < MAX_ITERATIONS)
 	{
 		t = now();
 		x_old = gs_mult_par(adjacency, &x, b, rows, row_sums);	
