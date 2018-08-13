a221e2db50cdc5afd8b5f91713a4640ee7b2a49f
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2018-08-13 19:04:47 +0300

    quicksort with hoares implementation

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -1,55 +1,48 @@
 #include <stdio.h>
+
 // A utility function to swap two elements
 void swap(int* a, int* b, int* i, int *j)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+	int t = *a;
+	*a = *b;
+	*b = t;
 
-    t = *i;
-    *i = *j;
-    *j = t;
+	t = *i;
+	*i = *j;
+	*j = t;
 }
- 
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
-int partition (int arr[], int index[], int low, int high)
+
+int
+partition(int *arr, int *index, int low, int high)
 {
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
- 
-    for (int j = low; j <= high- 1; j++)
-    {
-        // If current element is smaller than or
-        // equal to pivot
-        if (arr[j] <= pivot)
-        {
-            i++;    // increment index of smaller element
-            swap(&arr[i], &arr[j], &index[i], &index[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high], &index[i + 1], &index[high]);
-    return (i + 1);
+	int pivot = arr[low];
+	int i = low - 1;
+	int j = high + 1;
+
+	while(1)
+	{
+		do
+			i++;
+		while (arr[i] < pivot);
+
+		do
+			j--;
+		while (arr[j] > pivot);
+
+		if (i >= j)
+			return j;
+
+		swap(&arr[i], &arr[j], &index[i], &index[j]);
+	}
 }
- 
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
-void quickSort(int arr[], int index[], int low, int high)
+
+void quick_sort(int *arr, int *index, int low, int high)
 {
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[p] is now
-           at right place */
-        int pi = partition(arr, index, low, high);
- 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, index, low, pi - 1);
-        quickSort(arr, index, pi + 1, high);
-    }
+	if (low < high)
+	{
+		int pivot_index = partition(arr, index, low, high);
+
+		quick_sort(arr, index, low, pivot_index);
+		quick_sort(arr, index, pivot_index + 1, high);
+	}
 }
